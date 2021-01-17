32d21924cf60f10b50f991afcb45021248d0d4c8
zq99299 <99299684@qq.com>
2021-01-17 14:37:06 +0800

    堆和非堆 内存溢出 列子

---
+++ src/test/java/cn/mrcode/stady/monitor_tuning/MonitorTuningApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.mrcode.stady.monitor_tuning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorTuningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
